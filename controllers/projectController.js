b89092b4bfa8c993ac1d55560488f9eeb5a2c908
muteeurrehman28 <bsai23005@itu.edu.pk>
2025-05-18 22:09:31 +0500

    Add project controller

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -3,6 +3,8 @@ const Project = require('../models/Project');
 // @desc    Get all projects
 // @route   GET /api/projects
 // @access  Public
+//
+
 const getProjects = async (req, res) => {
   try {
     const projects = await Project.find().sort({ startDate: -1 });
